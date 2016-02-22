5de7daec9ff3fe0521f2e7cdf88aec99659df557
Francisco Guzmán <paco.mgh@hotmail.com>
2016-02-22 00:10:05 -0600

    initial scripts to move characters

---
+++ Assets/Scripts/ControlEnemigo.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ControlEnemigo : MonoBehaviour {
+
+    public float velocity = -1f;
+    Rigidbody2D rigidBody;
+
+
+	// Use this for initialization
+	void Start () {
+        rigidBody = GetComponent<Rigidbody2D>();
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        Vector2 v = new Vector2(velocity, 0);
+        rigidBody.velocity = v;
+	
+	}
+}
